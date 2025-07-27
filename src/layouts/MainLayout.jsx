5838914dc8709805117510e8318778dcd42085a7
Jamil Ahmed <what6499@gmail.com>
2025-07-28 03:18:44 +0600

    basic firebase int

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
 const MainLayout = () => {
-  return <div>asdasd</div>;
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>Overview</Tab>
+        <Tab>Packages</Tab>
+        <Tab>Tour Guides</Tab>
+      </TabList>
+
+      <TabPanel>
+        <p>This is the Overview section.</p>
+      </TabPanel>
+      <TabPanel>
+        <p>These are our travel packages.</p>
+      </TabPanel>
+      <TabPanel>
+        <p>Meet our tour guides.</p>
+      </TabPanel>
+    </Tabs>
+  );
 };
 
 export default MainLayout;
